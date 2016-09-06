f0f62a4fbc290b3bb75cbcf2de38dc5c458d26ab
Stephen Kall <stephenkall@gmail.com>
2016-09-06 13:29:40 -0300

    Adjusting spawn body parts per level

--- main.js
+++ main.js
@@ -3,12 +3,49 @@ var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 var roleRepairer = require('role.repairer');
 
-module.exports.loop = function () {
-    
-    var maxHarvesters = 5;
-    var maxUpgraders = 5;
-    var maxBuilders = 2;
-    var maxRepairers = 10;
+module.exports.loop = function ()
+{
+    var maxHarvesters = 1;
+    var maxUpgraders = 1;
+    var maxBuilders = 1;
+    var maxRepairers = 1;
+    var bodyParts = [WORK,CARRY,MOVE];
+    var curLevel = Game.spawns.Spawn1.room.controller.level;
+
+    switch(curLevel)
+    {
+        case 1:
+            maxHarvesters = 2;
+            maxUpgraders = 2;
+            maxBuilders = 1;
+            maxRepairers = 1;
+            bodyParts = [WORK,CARRY,MOVE];
+            break;
+            
+        case 2:
+            maxHarvesters = 2;
+            maxUpgraders = 2;
+            maxBuilders = 2;
+            maxRepairers = 2;            
+            bodyParts = [WORK,CARRY,MOVE];
+            break;
+            
+        case 3:
+            maxHarvesters = 3;
+            maxUpgraders = 3;
+            maxBuilders = 2;
+            maxRepairers = 10;
+            bodyParts = [WORK,WORK,CARRY,CARRY,MOVE,MOVE];
+            break;
+            
+        default:
+            maxHarvesters = 5;
+            maxUpgraders = 5;
+            maxBuilders = 5;
+            maxRepairers = 10;
+            bodyParts = [WORK,WORK,WORK,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE];
+            break;
+    }
 
     /*var tower = Game.getObjectById('88fa06c1f96d6e43c2eaef78');
     if(tower) {
@@ -24,13 +61,14 @@ module.exports.loop = function () {
             tower.attack(closestHostile);
         }
     }*/
-    
-    var hostiles = Game.rooms.E21S58.find(FIND_HOSTILE_CREEPS);
+
+    var hostiles = Game.spawns.Spawn1.room.find(FIND_HOSTILE_CREEPS);
     
     if(hostiles.length > 0) {
         var username = hostiles[0].owner.username;
-        Game.notify(`User ${username} spotted in room ${roomName}`);
-        var towers = Game.rooms[roomName].find(FIND_MY_STRUCTURES, {filter: {structureType: STRUCTURE_TOWER}});
+        //Game.notify('User ${username} spotted in room ${roomName}');
+        Game.notify('User ${username} spotted in room');
+        var towers = Game.spawns.Spawn1.room.find(FIND_MY_STRUCTURES, {filter: {structureType: STRUCTURE_TOWER}});
         towers.forEach(tower => tower.attack(hostiles[0]));
     }
 
@@ -47,52 +85,67 @@ module.exports.loop = function () {
     var repairers  = _.filter(Game.creeps, (creep) => creep.memory.role == 'repairer');
 
     if(harvesters.length < maxHarvesters) {
-        var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,WORK,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE], undefined, {role: 'harvester'});
-        if (newName.length > 2)
+        if (harvesters == 0) // Fresh start (all creeps dead)
         {
-            console.log('Spawning new harvester: ' + newName);
+            var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester', lvl: 1});
+            if (newName.length > 2)
+            {
+                console.log('Spawning new harvester: ' + newName + ', level: 1');
+            }           
+        }
+        else
+        {
+            var newName = Game.spawns['Spawn1'].createCreep(bodyParts, undefined, {role: 'harvester', lvl: curLevel});
+            if (newName.length > 2)
+            {
+                console.log('Spawning new harvester: ' + newName + ', level: ' + curLevel);
+            }
         }
     }
     else
     if(upgraders.length < maxUpgraders) {
-        var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,WORK,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE], undefined, {role: 'upgrader'});
+        var newName = Game.spawns['Spawn1'].createCreep(bodyParts, undefined, {role: 'upgrader', lvl: curLevel});
         if (newName.length > 2)
         {
-            console.log('Spawning new upgrader: ' + newName);
+            console.log('Spawning new upgrader: ' + newName + ', level: ' + curLevel);
         }
     }
     else
     if(builders.length < maxBuilders) {
-        var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,WORK,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE], undefined, {role: 'builder'});
+        var newName = Game.spawns['Spawn1'].createCreep(bodyParts, undefined, {role: 'builder', lvl: curLevel});
         if (newName.length > 2)
         {
-            console.log('Spawning new builder: ' + newName);
+            console.log('Spawning new builder: ' + newName + ', level: ' + curLevel);
         }
     }
     else
     if(repairers.length < maxRepairers) {
-        var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,WORK,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE], undefined, {role: 'repairer'});
+        var newName = Game.spawns['Spawn1'].createCreep(bodyParts, undefined, {role: 'repairer', lvl: curLevel});
         if (newName.length > 2)
         {
-            console.log('Spawning new repairer: ' + newName);
+            console.log('Spawning new repairer: ' + newName + ', level: ' + curLevel);
         }
     }
-    
-    harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-    upgraders  = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
-    builders   = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
-    repairers  = _.filter(Game.creeps, (creep) => creep.memory.role == 'repairer');
+
     //console.log('Harvesters: ' + harvesters.length);
     //console.log('Builders: '   + builders.length);
     //console.log('Upgraders: '  + upgraders.length);
     //console.log('Repairers: '  + upgraders.length);
     
     for(var name in Game.creeps) {
+
+        var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+        var upgraders  = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+        var builders   = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+        var repairers  = _.filter(Game.creeps, (creep) => creep.memory.role == 'repairer');
+        
         var creep = Game.creeps[name];
-        if(creep.memory.role == 'harvester') {
-            if (harvesters.length > maxHarvesters)
+        
+        if(creep.memory.role == 'harvester')
+        {
+            if (harvesters.length > maxHarvesters && harvesters.length > 1)
             {
-                console.log('Suiciding harvester: ' + creep.name);
+                console.log('Suiciding harvester: ' + creep.name + ', level: ' + creep.memory.lvl + '. Amount: ' + harvesters.length + '/' + maxHarvesters);
                 creep.suicide();
             }
             else
@@ -101,9 +154,10 @@ module.exports.loop = function () {
             }
         }
         if(creep.memory.role == 'upgrader') {
+
             if (upgraders.length > maxUpgraders)
             {
-                console.log('Suiciding upgrader: ' + creep.name);
+                console.log('Suiciding upgrader: ' + creep.name + ', level: ' + creep.memory.lvl + '. Amount: ' + upgraders.length + '/' + maxUpgraders);
                 creep.suicide();
             }
             else
@@ -112,9 +166,10 @@ module.exports.loop = function () {
             }
         }
         if(creep.memory.role == 'builder') {
+
             if (builders.length > maxBuilders)
             {
-                console.log('Suiciding builder: ' + creep.name);
+                console.log('Suiciding builder: ' + creep.name + ', level: ' + creep.memory.lvl + '. Amount: ' + builders.length + '/' + maxBuilders);
                 creep.suicide();
             }
             else
@@ -123,9 +178,10 @@ module.exports.loop = function () {
             }
         }
         if(creep.memory.role == 'repairer') {
+
             if (repairers.length > maxRepairers)
             {
-                console.log('Suiciding repairer: ' + creep.name);
+                console.log('Suiciding repairer: ' + creep.name + ', level: ' + creep.memory.lvl + '. Amount: ' + repairers.length + '/' + maxRepairers);
                 creep.suicide();
             }
             else
@@ -134,9 +190,8 @@ module.exports.loop = function () {
             }
         }
         if(creep.memory.role == null) {
-            console.log('Suiciding creep: ' + creep.name);
+            console.log('Suiciding creep with no role: ' + creep.name);
             creep.suicide();
         }
     }
 }
-
